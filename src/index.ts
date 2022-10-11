23875cf85188c4b12ef2688100a825a900a87f1f
Anton <nokker@gmail.com>
2022-10-11 21:11:05 +0900

    add gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.85.0/http/server.ts";
 
-let port = parseInt(Deno.env.get("PORT") ?? "8000")
+let port = parseInt(Deno.env.get("PORT") ?? "8000");
 const s = serve({ port });
 
 console.log(`http://localhost:${port}/`);
